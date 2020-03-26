4d85d0445fddb9926f73983e6556040b4d8a9dcb
John Biggers <johnbiggers@me.com>
2020-03-25 22:11:25 -0500

    q

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,11 +4,10 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is John</h1>
+                <h1>>></h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
+                <p>.</p>
             </div>
         </div>
     </section>
