02447f4b2122caccf40b49dbcc70c1b608c49abe
John Biggers <johnbiggers@me.com>
2020-03-27 18:26:34 -0500

    a

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,10 +6,10 @@ const Menu = (props) => (
     <nav id="menu">
         <div className="inner">
             <ul className="links">
-                <li><Link onClick={props.onToggleMenu} to="/">Home</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/landing">Landing</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/generic">Generic</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/elements">Elements</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/">HOME</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/about">ABOUT</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/year">YEAR</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/playlists">PLAYLISTS</Link></li>
             </ul>
             <ul className="actions vertical">
                 <li><a href="#" className="button special fit">Get Started</a></li>
