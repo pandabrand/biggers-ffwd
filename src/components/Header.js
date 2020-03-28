1fc42fb7c58476641a536eb9410e065eb36457fb
pandabrand <pandabrand@gmail.com>
2020-03-27 22:26:34 -0500

    eliminate menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = (props) => (
     <header id="header" className="alt">
         <Link to="/" className="logo"><strong>FFWD</strong> <span>>></span></Link>
         <nav>
-            <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
+            
         </nav>
     </header>
 )
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,18 +5,7 @@ import { Link } from 'gatsby'
 const Menu = (props) => (
     <nav id="menu">
         <div className="inner">
-            <ul className="links">
-                <li><Link onClick={props.onToggleMenu} to="/">HOME</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/about">ABOUT</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/year">YEAR</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/playlists">PLAYLISTS</Link></li>
-            </ul>
-            <ul className="actions vertical">
-                <li><a href="#" className="button special fit">Get Started</a></li>
-                <li><a href="#" className="button fit">Log In</a></li>
-            </ul>
         </div>
-        <a className="close" onClick={props.onToggleMenu} href="javascript:;">Close</a>
     </nav>
 )
 
